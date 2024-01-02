d3fa79f1ee219b7f9538892c24ead5b0bd7b3450
Sergueille <84402628+Sergueille@users.noreply.github.com>
2024-01-02 19:55:15 +0100

    Renamed folder

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 
 fn main() {
-    println!("cargo:rustc-link-lib=c/freetype/freetyped");
-    println!("cargo:rerun-if-changed=c/wrapper.h");
+    println!("cargo:rustc-link-lib=dependencies/freetype/freetyped");
+    println!("cargo:rerun-if-changed=dependencies/wrapper.h");
 
     let bindings = bindgen::Builder::default()
-        .header("c/wrapper.h")
+        .header("dependencies/wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
